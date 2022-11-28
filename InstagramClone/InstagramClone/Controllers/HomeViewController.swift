9b2d09b3e1e3c0946e30181e88a6a065e6954ae7
Aditya Jha <adityajha199@gmail.com>
2022-11-29 00:16:48 +0530

    Initial Project Setup

---
+++ InstagramClone/InstagramClone/Controllers/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Adi on 11/29/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
